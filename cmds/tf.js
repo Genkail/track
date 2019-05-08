40b078bc6973c4f9c387c019d35b6c7a5fd6553e
Genkail <50331122+Genkail@users.noreply.github.com>
2019-05-08 19:23:32 +1000

    Update tf.js

--- cmds/tf.js
+++ cmds/tf.js
@@ -38,17 +38,3 @@ module.exports.help= {
 
 name: "tf"
 } 
-hotboi.01.05.2019
-////emote-v2
-    var args = args.join(" ")
-     if(!args) return;
-    var emoji = client.emojis.find(x => x.name.includes(args))
-    if(!emoji) return;
-    var id = emoji.id
-    var name = client.emojis.find(x => x.id == id).name
-    var gif = emoji.animated
-    if(!gif){
-    message.channel.send(`<:${name}:${id}>`)
-    }else{
-    message.channel.send(`<a:${name}:${id}>`)
-    }
