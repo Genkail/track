6cc6316b6bc86c486a7ec5de91c6b6f2dc62a30a
Genkail <50331122+Genkail@users.noreply.github.com>
2019-05-09 18:41:38 +1000

    Update namech.js

--- cmds/namech.js
+++ cmds/namech.js
@@ -4,7 +4,7 @@ message.delete()
 let prefix = ["!"]
 const CustomArgs = message.content.slice(prefix.length + 13 + 1).split("; ")
 let type = ["text","voice"]
-if(type.indexOf(CustomArgs[1]) == -1) return bot.send("Я не могу создать канал с таким типом, используйте !createchannel <ИМЯ> <text или voice>")
+if(type.indexOf(CustomArgs[1]) == -1) return bot.send("Я не могу создать канал с таким типом, используйте !createchannel <ИМЯ>; <text или voice>")
 if(!CustomArgs[0] | !CustomArgs[1]) return bot.send("У вас нет одного из значений!")
 message.guild.createChannel(CustomArgs[0], CustomArgs[1])
 };
