d68a3b201a5d92baf84aada5c19991b85ad232d8
Genkail <50331122+Genkail@users.noreply.github.com>
2019-05-17 20:25:37 +1000

    Update bredovyha.js

--- cmds/bredovyha.js
+++ cmds/bredovyha.js
@@ -2,13 +2,12 @@ const Discord = require("discord.js");
 
 
 module.exports.run = async (bot, message, args) => {
-
     if(!args[0]) return bot.send("Вы не указали Ведущего");
     let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if(!rUser) return message.channel.send("Не удалось найти пользователя.");
-    let cod = args.join(" ").slice(22);
-    if(!cod) return message.channel.send("вы не указали Код");
-    let time = args.join(" ").slice(23);
+    let rreason = args.join(" ").slice(22);
+    if(!rreason) return message.channel.send("вы не указали Код");
+     let time = args.join(" ").slice(23);
     if(!time) return message.channel.send("вы не указали время");
 
 
@@ -19,7 +18,7 @@ module.exports.run = async (bot, message, args) => {
     .addField("Ведущий", `${rUser}`)
 
     .setDescription("Игра, в которой надо придумывать ложные варианты ответа и одновременно искать правдивые.\nСайт для игры https://jackbox.fun \nСобираемся тут https://discord.gg/JsZUuGn (P.S позже перейдем в лс звонок)")
-    .addField("Код для участия", cod)
+    .addField("Код для участия", rreason)
     .setImage("https://jackbox.whatif.one/wp-content/uploads/2019/02/BannerFibbage3-750x410.png")
 
 let rpchannel = message.guild.channels.find('name', '🎉ивенты')
