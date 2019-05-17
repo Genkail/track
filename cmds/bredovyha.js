5d2941fa1b5ea4a1d6ceb82633ad48c6f244134c
Genkail <50331122+Genkail@users.noreply.github.com>
2019-05-17 20:45:16 +1000

    Update bredovyha.js

--- cmds/bredovyha.js
+++ cmds/bredovyha.js
@@ -6,7 +6,7 @@ module.exports.run = async (bot, message, args) => {
     if(!args[0]) return bot.send("Вы не указали Ведущего");
     let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if(!rUser) return message.channel.send("Не удалось найти пользователя.");
-    let cod = args.slice(1).join(" ");
+    let cod = args[1]
     if(!cod) return message.channel.send("вы не указали Код");
     let time = args.slice(2).join(" ");
     if(!time) return message.channel.send("вы не указали время");
