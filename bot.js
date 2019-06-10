3be9bdae6b868d4db3c3486ebe488dc11a8ec930
Glitch (genkail-track) <none>
2019-06-10 08:14:11 +0000

    👑🚉 Checkpoint ./bot.js:4777360/318 ./mutes.json:4777360/72

--- bot.js
+++ bot.js
@@ -282,11 +282,12 @@ bot.on("message", (message)=>{
     
 var foo = {};
 bot.on("message",(message)=>{
-    let BotR = message.guild.roles.find(r => r.name === 'Бот');
+ 
     if(message.channel.id=="565164893867737144") return;
     if(message.channel.id=="496915902063837184") return;
     if(message.author.id=="267967915855314944") return;
-    if(message.member.roles.has(BotR.id))return;
+    if(message.member.roles == null) return;
+    if(message.member.roles.has("483246707740639251"))return;
     
     if(!foo[message.author.id]) { foo[message.author.id] = 0; }
   if(!message.guild.member(message.author).roles.find(r => r.name == "Muted")){
