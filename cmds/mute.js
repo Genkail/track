3788614e37876579015902544f596dd46fe52e7f
Glitch (genkail-track) <none>
2019-07-22 08:05:29 +0000

    ⛪️🚡 Checkpoint ./mutes.json:4777360/2 ./cmds/mute.js:4777360/2884

--- cmds/mute.js
+++ cmds/mute.js
@@ -1,11 +1,15 @@
 const Discord = module.require("discord.js");
 const fs = require("fs");
+const ms = require("ms");
 module.exports.run = async (bot,message,args) => {
     if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("У вас нет прав");
     let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if(!args[0]) return bot.send("Вы не указали пользователя");
     if(!rUser) return bot.send("Пользователь не найден");
-    if(!args[1]) return bot.send("Укажите время в секундах");
+    if(!args[1]) return bot.send("Укажите время");
+    let rreason = args.slice(2).join(" ");
+    if(!rreason) return message.channel.send("вы не указали причину");
+let    mutetime=ms(args[1]);
     let role = message.guild.roles.find(r => r.name === "Muted");
     if(!role){
         role = await message.guild.createRole({
@@ -22,8 +26,9 @@ module.exports.run = async (bot,message,args) => {
     if(rUser.roles.has(role.id)) return bot.send("Этот пользователь уже не может говорить");
     bot.mutes[rUser.id] = {
         guild:message.guild.id,
-        time:parseInt(Date.now() + (args[1]*1000)),
+        time:parseInt(Date.now() + (mutetime)),
     };
+    bot.send(`${rUser} замучен на ${args[1]} за ${rreason} `);
     fs.writeFile('./mutes.json',JSON.stringify(bot.mutes),(err)=>{
         if(err) console.log(err);
     });
