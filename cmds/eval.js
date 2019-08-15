f9c0be085f7c72d1d0f2066eac240eed57b968f8
Glitch (genkail-track) <none>
2019-08-15 15:48:47 +0000

    🛫😼 Checkpoint ./cmds/eval.js:4777360/3021

--- cmds/eval.js
+++ cmds/eval.js
@@ -1,5 +1,4 @@
 const Discord = require("discord.js");
-const db = require("quick.db")
 module.exports.run = async (bot, message, args) => {
 if (message.author.id == "428464195458170881")return bot.send("У тебя есть доступ... Но слушать тебя не буду");
     if ( message.author.id !=="267967915855314944") return bot.send("<a:zarezhy:609363605032534016>");
@@ -16,9 +15,7 @@ message.delete().catch();
         .setColor('RANDOM')
         .addField(':inbox_tray: До', `\`\`\`js\n${codein}\`\`\``)
         .addField(':outbox_tray: После', `\`\`\`js\n${code}\n\`\`\``)
-let guild = bot.guilds.get("603486182814515206")
-        let rpchannel = guild.channels.find('name', 'eval')
-rpchannel.send(embed)
+
     } catch(e) {
         message.channel.send(`\`\`\`js\n${e}\n\`\`\``);
     }
